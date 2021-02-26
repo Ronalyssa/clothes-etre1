57dbca576eca9af7d98f4c500f81571002dac32f
ronalyssa <ronalyssa99@gmail.com>
2021-02-26 08:26:08 -0600

    login is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,30 @@ class UsersController < ApplicationController
       render json: user, status: 200
     end
     
+    # def create
+    #   #  byebug
+    #   # puts "hello"
+    #   user = User.new(user_params)
+    #   if user.save 
+    #     render json: user, except: [:created_at, :updated_at]
+    #   else
+    #     render json: [error: "Something went wrong."]
+    #   end
+    # end
+
+    def login
+      user = User.find_by({username: params[:username]})
+      if user && user.authenticate(params[:password])
+          render json: user, include: [:tops, :bottoms]
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+        
+   
     
 end
